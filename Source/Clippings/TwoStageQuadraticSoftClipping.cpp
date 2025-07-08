a0eba1a0975b92afaeee43736e352c138b28afe4
Bao Vuong <vqbao@lv5.org>
2025-07-08 01:56:43 -0400

    implemented factor for TSQ

--- Source/Clippings/TwoStageQuadraticSoftClipping.cpp
+++ Source/Clippings/TwoStageQuadraticSoftClipping.cpp
@@ -14,26 +14,29 @@
 
 void TwoStageQuadraticSoftClipping::clip(float* samples, int sampleIndex, float factor)
 {
-    
-    float a = 1.0f / 3.0f;
-    float b = 2.0f / 3.0f;
 
-    float scaled = Helper::scale(factor, 2, 5);
+    float scaled = Helper::scale(factor, 0, 0.5);
     int sign = Helper::sgn(samples[sampleIndex]);
     float absValue = abs(samples[sampleIndex]);
 
+    float twoThirds = 2.0f / 3.0f;
+
+    float a = scaled;
+    float b = log10f(3 - 6 * a) / log10f(2 - abs(3 * a));
+
+
     // TODO there is a lot of variables that the factor knob could control. let's figure out what to do
-    // * the piecewise function boundary values
+    // [x] the piecewise function boundary values
     // * in the second condition block, some subtraction values could work
     // * in the second condition block, maybe the base value for pow(n - abs(m))
 
-    if (absValue > b)
+    if (absValue > twoThirds)
     {
         samples[sampleIndex] = sign;
     }
-    else if (Helper::isBetween(absValue, a, b))
+    else if (Helper::isBetween(absValue, a, twoThirds))
     {
-        samples[sampleIndex] = sign * ((3 - pow(2 - abs(3 * samples[sampleIndex]), 2)) / 3.0f);
+        samples[sampleIndex] = sign * ((3 - pow(2 - abs(3 * samples[sampleIndex]), b)) / 3.0f);
     }
     else if (absValue < a)
     {
